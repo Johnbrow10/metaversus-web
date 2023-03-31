d6d9aebf22b622f1f170a4821bcf14d586eb6510
johnbrow10 <iroman250@gmail.com>
2023-03-31 18:47:45 -0300

    feat(metaversos): adicionar a arquitetura do projeto e suas configurações
    adicionanr os arquivos de componenets, containers, estilizações, configurações do nextJs, tailwind,
    postcss e husk com comitzen

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'O Mundo Invertido',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Perumahan Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paraiso Islândia',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Laboratórios Hawkins',
+  },
+];
+
+export const startingFeatures = [
+  'Encontre um mundo que combina com você e que você deseje entrar',
+  'Entre no mundo lendo a bíblia para estar seguro',
+  'Não há necessidade de rodeios, apenas fique no acelerador e divirta-se',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Um Novo Mundo',
+    subtitle:
+        'Temos a atualização mais recente com o novo mundo para você tentar, não importa',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Mais realismo',
+    subtitle:
+        'Na atualização mais recente, seus olhos estão estreitos, tornando o mundo mais realista do que nunca',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Tlançamento do Metaverso faz tremer Elon Musk',
+    subtitle:
+        'O Metaverso é um conceito que se refere a um universo virtual compartilhado, onde os usuários podem interagir e realizar atividades em um ambiente digitalizado em 3D',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 dicas para dominar facilmente a loucura do Metaverso',
+    subtitle:
+        'Experimente diferentes plataformas, Aprenda as habilidades básicas de navegação, conecte-se com outras pessoas, aprenda a criar conteúdo, tenha segurança, experimente atividades diferentes, esteja aberto a novas experiencias',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Com uma plataforma, você pode explorar o mundo inteiro virtualmente',
+    subtitle:
+        'É uma espécie de evolução da Internet, que permite uma experiência mais imersiva e colaborativa, além de possibilitar a criação de novos ecossistemas digitais.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
